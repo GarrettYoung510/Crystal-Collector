05e88c88a53c7f47cd95dfa258a21ef9da42cf65
Garrett Young <garrettyoung510@gmail.com>
2019-05-27 13:06:06 -0700

    split javascript into game.js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+$(document).ready(function gameStart() {
+    // variables for the game
+    // create variable for holding score
+    let points = 0;
+    // variable for target points
+    // set target score to be random up to 77
+    let targetPoints = Math.floor(Math.random() * 77);;
+    // variable holds losses
+    let losses = 0;
+    // variable holds wins
+    let wins = 0;
+    // sets target total for user to see
+    $('#target-points').text(targetPoints);
+    // variable for crystal value
+    let crystalValue = 0;
+
+    // create on click function for clicking crystals
+    $("#crystal1,#crystal2,#crystal3,#crystal4").on("click", function() {
+        // variable for holding the value of selected crystal
+        crystalValue = $(this).attr('value');
+        // adds to points when clicking a crystal
+        points += parseInt(crystalValue);
+        // changes total on the screen
+        $('#points').text(points);
+
+        if (points === targetPoints) {
+            // win condition and alert
+            alert("You Win! GOOD JOB!");
+            // win
+            wins++;
+            // updates wins for user to see
+            $('#wins').text(wins);
+            // selects new number to guess
+            targetPoints = Math.ceil(Math.random() * 77);
+            $('#target-points').text(targetPoints);
+            // reset points
+            points = 0;
+            $('#points').text(points);
+
+        } else if (points >= targetPoints) {
+            // lose condition and alert
+            alert("You lose. Please try again!");
+            // lose
+            losses++;
+            // updates losses for user to see
+            $('#losses').text(losses);
+            // selects new number to guess
+            targetPoints = Math.ceil(Math.random() * 77);
+            $('#target-points').text(targetPoints);
+            // reset points
+            points = 0;
+            $('#points').text(points);
+
+        } else {
+            // alert("Please select another crystal!");
+            // keep going
+        }
+    });
+
+});
